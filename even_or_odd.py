83231d92767320c9403088c009f33701259ecfc6
LEWIS NTEGEREJIMANA <142150994+umulewi@users.noreply.github.com>
2024-04-12 00:25:59 +0200

    odd or even
    after adding odd or even i also arrange the program files name

---
+++ even_or_odd.py
@@ -0,0 +1,5 @@
+a=int (input("enter a number "))
+if a%2==0:
+    print("a number is even")
+else:
+    print("a number is odd")
--- third.py
+++
@@ -1,4 +0,0 @@
-a=(input("enter a first numbers "))
-b=(input("enter second number "))
-sum=a+b;
-print(sum)
