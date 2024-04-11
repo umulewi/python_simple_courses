0e1d831839fd2bfef0bf30aed2b506ba18897720
LEWIS NTEGEREJIMANA <142150994+umulewi@users.noreply.github.com>
2024-04-12 00:48:15 +0200

    Create display_even_number_between_numbers.py

---
+++ display_even_number_between_numbers.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(" the even number between ", a,"And", b,"are:")
+for i in range(a, b+1):
+    if i%2==0:
+        print(i)
