8ce8297321bbe89576d317f80d74c5f4596f7b7a
LEWIS NTEGEREJIMANA <142150994+umulewi@users.noreply.github.com>
2024-04-13 01:14:19 +0200

    Update record_10_seperate_numbers_using_python.py

--- record_10_seperate_numbers_using_python.py
+++ record_10_seperate_numbers_using_python.py
@@ -1,1 +1,15 @@
-a=int(input("enter a 10 numbers"))
+def findmin():
+    a=int(input("enter a 10 numbers"))
+    max=a
+    min=a
+    i=2
+    while i<=5:
+        x=int(input("enter a number"))
+        if min>a:
+            min=a
+        elif max<a:
+            max=a
+        i=i+1
+    print(max)
+    print(min)
+findmin()
