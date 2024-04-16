7a4c008a7acf4d143dc8fddbcb74e68cf27b792c
LEWIS NTEGEREJIMANA <umulewi@gmail.com>
2024-04-16 14:55:24 -0700

    Create array_function.py

---
+++ array_function.py
@@ -0,0 +1,9 @@
+def display_array(arr):
+    for element in arr:
+        print(element)
+
+# Define an array
+my_array = [1, 2, 3, 4, 5]
+
+# Call the function to display the array
+display_array(my_array)
